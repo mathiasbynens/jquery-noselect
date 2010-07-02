218d8e45481422550a4b82a0c6178049b6c7142b
Mathias Bynens <mathias@qiwi.be>
2010-07-02 13:39:07 +0200

    Improved compression/minification

--- jquery.noselect.js
+++ jquery.noselect.js
@@ -1,5 +1,5 @@
 /*!
- * noSelect jQuery Plugin
+ * noSelect jQuery Plugin v1.0
  * @link http://github.com/mathiasbynens/noSelect-jQuery-Plugin
  * @author Mathias Bynens <http://mathiasbynens.be/>
  */
@@ -9,10 +9,12 @@
    return false;
   };
   return this.each(function() {
+   // Since the string 'none' is used three times, storing it in a variable gives better results after minification
+   var none = 'none';
    this.onselectstart = this.ondragstart = f; // Webkit & IE
    $(this)
     .mousedown(f) // Webkit & Opera
-    .css({ MozUserSelect: 'none', WebkitUserSelect: 'none', userSelect: 'none' });
+    .css({ MozUserSelect: none, WebkitUserSelect: none, userSelect: none });
   });
  };
 })(jQuery);
