d923b4e593092c109dabd8118be5c2ac109d162c
Mathias Bynens <mathias@qiwi.be>
2010-10-16 04:30:52 -0700

    Switched to tabs for indentation

--- jquery.noselect.js
+++ jquery.noselect.js
@@ -1,20 +1,20 @@
 /*!
- * noSelect jQuery Plugin v1.0
- * @link http://github.com/mathiasbynens/noSelect-jQuery-Plugin
- * @author Mathias Bynens <http://mathiasbynens.be/>
- */
+* noSelect jQuery Plugin v1.0
+* @link http://github.com/mathiasbynens/noSelect-jQuery-Plugin
+* @author Mathias Bynens <http://mathiasbynens.be/>
+*/
 ;(function($) {
- $.fn.noSelect = function() {
-  function f() {
-   return false;
-  };
-  return this.each(function() {
-   // Since the string 'none' is used three times, storing it in a variable gives better results after minification
-   var none = 'none';
-   this.onselectstart = this.ondragstart = f; // Webkit & IE
-   $(this)
-    .mousedown(f) // Webkit & Opera
-    .css({ MozUserSelect: none, WebkitUserSelect: none, userSelect: none });
-  });
- };
+	$.fn.noSelect = function() {
+		function f() {
+			return false;
+		}
+		return this.each(function() {
+			// Since the string 'none' is used three times, storing it in a variable gives better results after minification
+			var none = 'none';
+			this.onselectstart = this.ondragstart = f; // Webkit & IE
+			$(this)
+				.mousedown(f) // Webkit & Opera
+				.css({ MozUserSelect: none, WebkitUserSelect: none, userSelect: none });
+		});
+	};
 })(jQuery);
