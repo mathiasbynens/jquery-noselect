051499647dc34e3400b884cfad420c091643f5c7
Mathias Bynens <mathias@qiwi.be>
2011-02-20 14:32:19 +0100

    Get rid of unneeded closure and save some bytes.

--- jquery.noselect.js
+++ jquery.noselect.js
@@ -3,18 +3,16 @@
 * @link http://github.com/mathiasbynens/noSelect-jQuery-Plugin
 * @author Mathias Bynens <http://mathiasbynens.be/>
 */
-;(function($) {
-	$.fn.noSelect = function() {
-		function f() {
-			return false;
-		}
-		return this.each(function() {
-			// Since the string 'none' is used three times, storing it in a variable gives better results after minification
-			var none = 'none';
-			this.onselectstart = this.ondragstart = f; // Webkit & IE
-			$(this)
-				.mousedown(f) // Webkit & Opera
-				.css({ MozUserSelect: none, WebkitUserSelect: none, userSelect: none });
-		});
-	};
-})(jQuery);
+jQuery.fn.noSelect = function() {
+	function f() {
+		return false;
+	}
+	return this.each(function() {
+		// Since the string 'none' is used three times, storing it in a variable gives better results after minification
+		var none = 'none';
+		this.onselectstart = this.ondragstart = f; // WebKit & IE
+		jQuery(this)
+			.mousedown(f) // WebKit & Opera
+			.css({ MozUserSelect: none, WebkitUserSelect: none, userSelect: none });
+	});
+};
