6cb79df5fe93c01bdef6ea2e226b821a17704ccd
Mathias Bynens <mathias@qiwi.be>
2011-06-06 10:18:45 +0200

    Remove unused function (forgot that earlier). #facepalm

--- jquery.noselect.js
+++ jquery.noselect.js
@@ -1,15 +1,12 @@
 /*!
-* noSelect jQuery Plugin v1.0.1
+* noSelect jQuery Plugin v1.0.2
 * @link http://mths.be/noselect
 * @author Mathias Bynens <http://mathiasbynens.be/>
 */
 jQuery.fn.noSelect = function() {
 
 	// Since the string 'none' is used three times, storing it in a variable gives better results after minification
-	var none = 'none',
-	    f = function() {
-	    	return false;
-	    };
+	var none = 'none';
 
 	// onselectstart and ondragstart for WebKit & IE
 	// onmousedown for WebKit & Opera
